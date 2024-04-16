af94234b5485633a814b860d123a138629fd584a
gekkekoe <gekkekoe@gmail.com>
2024-04-16 13:54:04 +0200

    move esp32s3 config to separate config file to allow regular esp32 builds

--- components/ecodan/ecodan.h
+++ components/ecodan/ecodan.h
@@ -8,7 +8,6 @@
 #include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
 #include "proto.h"
