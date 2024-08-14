99015381110ad1194a737f3514661ade7c41b203
gekkekoe <gekkekoe@gmail.com>
2024-08-14 15:23:37 +0200

    Revert "only publish changes when state actually changes (except binary sensors)"
    This reverts commit def96a9d1a15f9dae96552b254cfd34eb54c3c31.
    
    causes some random reboots

--- components/ecodan/ecodan.cpp
+++ components/ecodan/ecodan.cpp
@@ -11,8 +11,7 @@ namespace ecodan
     void EcodanHeatpump::publish_state(const std::string& sensorKey, float sensorValue) {
         auto sensor_it = sensors.find(sensorKey);
         if (sensor_it != sensors.end()) {
-            if (!sensor_it->second->has_state() || sensor_it->second->get_state() != sensorValue)
-                sensor_it->second->publish_state(sensorValue);
+            sensor_it->second->publish_state(sensorValue);
         } 
         else 
         {
@@ -23,8 +22,7 @@ namespace ecodan
     void EcodanHeatpump::publish_state(const std::string& sensorKey, const std::string& sensorValue) {        
         auto textSensor_it = textSensors.find(sensorKey);
         if (textSensor_it != textSensors.end()) {
-            if (!textSensor_it->second->has_state() || textSensor_it->second->get_state() != sensorValue)
-                textSensor_it->second->publish_state(sensorValue);
+            textSensor_it->second->publish_state(sensorValue);
         }
         else 
         {
