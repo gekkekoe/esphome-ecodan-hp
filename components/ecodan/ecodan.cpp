fd2234bb2d3a288d8dbf84d2202bb2802d640f0a
gekkekoe <gekkekoe@gmail.com>
2024-09-23 14:03:06 +0200

    test respond on 0x02

--- components/ecodan/ecodan.cpp
+++ components/ecodan/ecodan.cpp
@@ -86,7 +86,7 @@ namespace ecodan
 
     void EcodanHeatpump::loop()
     {
-        if (uart_ && uart_->available() && serial_rx(uart_, res_buffer_))
+        if (uart_ && serial_rx(uart_, res_buffer_))
         {
             #ifdef REVERSE_EGINEER
             ESP_LOGI(TAG, res_buffer_.debug_dump_packet().c_str());
@@ -99,8 +99,16 @@ namespace ecodan
             res_buffer_ = Message();
         }
 
-        if (proxy_uart_ && proxy_uart_->available() && serial_rx(proxy_uart_, proxy_buffer_))
+        if (proxy_uart_  && serial_rx(proxy_uart_, proxy_buffer_))
         {
+
+            // Test try to handle 0x00 0xff 0xff sequences
+            if (proxy_buffer_.buffer()[0] == 0x02) {
+                uint8_t fake_response[18] = { 0x02, 0xFF, 0xFF, 0x80, 0x00, 0x00, 0x0A, 0x01, 0x00, 0x40, 0x00, 0x00, 0x06, 0x02, 0x7A, 0x00, 0x00, 0xB5 };
+                proxy_uart_->flush();
+                proxy_uart_->write_array(fake_response, 18);
+            }
+
             // forward cmds from slave to master
             serial_tx(uart_, proxy_buffer_);
 
