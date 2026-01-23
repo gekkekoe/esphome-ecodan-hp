d4a9a00855a3fd22d4e1fff1b4c72b02f8a919d3
bwduncan <bwduncan@gmail.com>
2026-01-23 13:53:01 +0000

    Use .c_str() with StringRef for 2026.1
    Fix breaking change in 2026.1
    
    This fixes a compile error, tested with 2026.1.1.
    
    (But it should probably be using parse_number rather than atoi...)

--- components/optimizer/prevention.cpp
+++ components/optimizer/prevention.cpp
@@ -214,7 +214,7 @@ namespace esphome
             flag |= esphome::ecodan::CONTROLLER_FLAG::SERVER_CONTROL;
             this->state_.ecodan_instance->set_controller_mode(flag, true);
 
-            uint32_t duration_s = atoi(this->state_.lockout_duration->current_option()) * 60UL;
+            uint32_t duration_s = atoi(this->state_.lockout_duration->current_option().c_str()) * 60UL;
             if (duration_s > 0)
             {
                 this->state_.lockout_expiration_timestamp = (uint32_t)(current_timestamp + duration_s);
